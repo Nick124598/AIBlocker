8ca58da2475258c321caa54b8fed5cc352e7192f
Nick Spindler <nickspindler08@gmail.com>
2026-01-31 16:11:49 +0200

    feat: added setting dns server and registering to the server

--- UIStudent/Form1.cs
+++ UIStudent/Form1.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Text.Json;
@@ -11,6 +13,21 @@ namespace UIStudent
             InitializeComponent();
         }
 
+
+        void SetDns(string interfaceName, string dns)
+        {
+            var psi = new ProcessStartInfo
+            {
+                FileName = "netsh",
+                Arguments = $"interface ip set dns name=\"{dns}\" static {interfaceName}",
+                Verb = "runas",   // forces admin
+                UseShellExecute = true,
+                CreateNoWindow = true
+            };
+
+            Process.Start(psi);
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
             const int registeration_server_port = 5000;
@@ -19,11 +36,31 @@ namespace UIStudent
                 Username = textBox1.Text
             };
             string jsonData = JsonSerializer.Serialize(data);
-            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            socket.Connect(textBox2.Text, registeration_server_port);
+            var ip = IPAddress.Parse(textBox2.Text);
+            var remoteEP = new IPEndPoint(ip, registeration_server_port);
+
+            using var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect(remoteEP);
+
             byte[] bytes = Encoding.UTF8.GetBytes(jsonData);
             socket.Send(bytes);
-            socket.Close();
+
+            bytes = new byte[1024];
+            socket.Receive(bytes);
+            string response = Encoding.UTF8.GetString(bytes).TrimEnd('\0');
+            if (response == "OK")
+            {
+                MessageBox.Show("Registration successful! Good luck at the sadna!");
+                SetDns("Wi-Fi", textBox2.Text);
+                socket.Close();
+            }
+            else
+            {
+                MessageBox.Show("Registration failed: " + response);
+                socket.Close();
+                throw new Exception("Registration failed: " + response);
+            }
+            
         }
     }
 }
